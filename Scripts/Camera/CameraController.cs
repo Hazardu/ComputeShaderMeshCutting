7da556084d2963290a5a0013590d6528e7ba8d24
Hazardu <42750285+Hazardu@users.noreply.github.com>
2019-09-25 15:50:09 +0200

    Initial Push

---
+++ Scripts/Camera/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform PlayerTr;
+
+    public Vector3 offset;
+    public float smoothing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+            transform.position = PlayerTr.position + offset;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (smoothing > 0)
+        {
+            transform.position = Vector3.Slerp(transform.position, PlayerTr.position + offset, Time.deltaTime * smoothing);
+        }
+        else
+        {
+            transform.position = PlayerTr.position + offset;
+        }
+    }
+}
